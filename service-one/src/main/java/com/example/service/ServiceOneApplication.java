7207e213f1e09a82e06eae8801be3b7ef491cf3e
deepakbhalla <deepak.dbhalla@gmail.com>
2020-09-01 01:40:21 +0530

    initial commit

---
+++ service-one/src/main/java/com/example/service/ServiceOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOneApplication.class, args);
+	}
+
+}
